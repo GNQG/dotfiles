501d979ce7615faa2829d6faedf9679fddabd5a4
GNQG <my.t.gnq+dev@gmail.com>
2019-06-14 13:39:38 +0900

    change method to load PowerShell profile

---
+++ shell/pwsh/profile-core.ps1
@@ -0,0 +1,5 @@
+. $PSScriptRoot\git.ps1
+. $PSScriptRoot\chocolatey.ps1
+. $PSScriptRoot\rustlang.ps1
+
+. $PSScriptRoot\ui.ps1
--- shell/pwsh/profile.ps1
+++ shell/pwsh/profile.ps1
@@ -1,5 +1,4 @@
-. $PSScriptRoot\git.ps1
-. $PSScriptRoot\chocolatey.ps1
-. $PSScriptRoot\rustlang.ps1
-
-. $PSScriptRoot\ui.ps1
+$profile_core = "$Env:DOTFILES_ROOTDIR\shell\pwsh\profile-core.ps1"
+if (Test-Path $profile_core) {
+	. $profile_core
+}
