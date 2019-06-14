ebe6fb324f3d9ae35b039438fa6bbb5e400e5f8b
GNQG <my.t.gnq+dev@gmail.com>
2019-06-14 13:09:06 +0900

    update UI for PowerShell

--- PowerShell/ui.psm1
+++ PowerShell/ui.psm1
@@ -1,7 +1,16 @@
 if ($host.Name -cne 'Windows PowerShell ISE Host') {
-    Import-Module PSColor
+    #Import-Module PSColor
+    if (Get-Module -ListAvailable -Name DirColors) {
+        Import-Module DirColors
+    } else {
+        Install-Module DirColors -Scope CurrentUser
+        Import-Module DirColors
+    }
+    Import-Module PSReadLine
     Import-Module oh-my-posh
 
+    Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+    Update-Dircolors ~\dotfiles\.dir_colors\dircolors.ansi-dark
     $ThemeSettings.MyThemesLocation = "$PSScriptRoot\Themes"
     Set-Theme MyTheme
 }
