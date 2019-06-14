c4487de85564cbd35d29b7cf6681bfe0ecb9172e
GNQG <my.t.gnq+dev@gmail.com>
2019-06-14 17:31:10 +0900

    use DOTPATH instead of DOTFILES_ROOTDIR

--- shell/pwsh/ui.ps1
+++ shell/pwsh/ui.ps1
@@ -10,7 +10,7 @@ if ($host.Name -cne 'Windows PowerShell ISE Host') {
     Import-Module oh-my-posh
 
     Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-    Update-Dircolors ~\dotfiles\.dir_colors\dircolors.ansi-dark
+    Update-Dircolors $Env:DOTPATH\.dir_colors\dircolors.ansi-dark
     $ThemeSettings.MyThemesLocation = "$PSScriptRoot\Themes"
     Set-Theme MyTheme
 }
